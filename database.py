9a135f87a98fa447c3af74db4a103bb86b9e80b1
Mirna hatem <85480715+mirna-hatemm@users.noreply.github.com>
2023-12-22 17:55:39 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,25 @@
 import mysql.connector
+import os
 
+# Read database configuration from environment variables with default values
+db_host = os.getenv('DB_HOST', 'localhost')
+db_user = os.getenv('DB_USER', 'root')
+db_password = os.getenv('DB_PASSWORD', 'root')
+db_name = os.getenv('DB_NAME', 'user')
+print(f"Database Host: {db_host}")
+print(f"Database User: {db_user}")
+print("Database Password: [HIDDEN]")
+print(f"Database Name: {db_name}")
+# Connect to the database
 db = mysql.connector.connect(
-    host="mysql-container",
-    user="root",
-    password="root",
-    database="user"
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
 )
-
 
 def get_cursor():
     return db.cursor()
-
 
 def commit():
     try:
@@ -20,4 +29,3 @@ def commit():
         db.rollback()
         print("Error occurred during commit. Changes rolled back.")
         print("Error message:", str(e))
-
